f195632a7d4b68ef18001b64f78ce06a69fb6f19
Jue Wang <jue@uw.edu>
2023-02-16 11:55:36 -0800

    added back **kwargs to aa_model.write_traj() to allow ligand to be on different chain, so benchmark pipeline can run correctly

--- aa_model.py
+++ aa_model.py
@@ -606,7 +606,7 @@ def write_traj(path, xyz_stack, seq, bond_feats, **kwargs):
     xyz23 = pad_dim(xyz_stack, 2, 23)
     with open(path, 'w') as fh:
         for i, xyz in enumerate(xyz23):
-            rf2aa.util.writepdb_file(fh, xyz, seq, bond_feats=bond_feats[None], modelnum=i)
+            rf2aa.util.writepdb_file(fh, xyz, seq, bond_feats=bond_feats[None], modelnum=i, **kwargs)
 
 def minifier(argument_map):
     argument_map['out_9'] = None
